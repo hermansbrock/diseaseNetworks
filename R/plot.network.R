5bc390e2350d980d4f699ab54e8d98dec9d24293
hermansbrock <hermans.brock@gmail.com>
2014-12-16 14:11:32 +1030

    Plotting code
    Just playing around with the plotting code

--- R/plot.network.R
+++ R/plot.network.R
@@ -9,31 +9,27 @@
 #' @export
 #' @note 29 August 2014
 #' @examples
-#' network <- networkSimulate(0.1,10)
+#' network <- network(10,0.5)
 #' plot(network)
-plot.network <- function(x,size=4,...) {
+plot.network <- function(x,size=4,...){
   require(ggplot2)  
-  df = x$points
+  df = x$nodes
+  df <- data.frame(cbind(df,x$node_states))
+  colnames(df) <- c("x","y","s1")
   df$s1[df$s1==0] = "Susceptible"
   df$s1[df$s1==2] = "Recovered"
   df$s1[df$s1==1] = "Infected"
+  x$edges <- x$edges+1
+  
   from <- df[x$edges[,1],]
   to <- df[x$edges[,2],]
   segments = data.frame(x1=from$x,y1=from$y,x2 = to$x,y2=to$y)
   p = ggplot(aes(x,y,col=s1),data=df)
-  #
-  # If we have subgraphs identified, plot the network with subgraphs identified
-  if(!is.null(x$graph)){
-    segments$graph = factor(x$graph[x$edges[,1],2])
-    p = p + 
-      geom_segment(aes(x=x1,y=y1,xend=x2,yend=y2,col=graph),data=segments) +
-      labs(col="Subgraphs")
-  }
-  #
-  # If we don't know the subgraphs, do a simple plot
-  else {p <- p +
-          geom_segment(aes(x=x1,y=y1,xend=x2,yend=y2),data=segments,col="black") +
-          labs(col="Status")}
-  p <- p + geom_point(size=size) + theme_bw()
+  p <- p +
+    geom_segment(aes(x=x1,y=y1,xend=x2,yend=y2),data=segments,col="black") +
+    labs(col="Status")
+  p <- p + geom_point(size=size) + theme_bw()+xlim(0,1)+ylim(0,1)
   return(p)  
 }
+
+
